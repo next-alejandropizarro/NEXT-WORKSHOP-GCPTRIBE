15f536e15a4ec0c3232c8aad68e9601b31b3046e
Rubén Escabia <ruben.escabia.beeva@xbyorange.com>
2019-06-13 08:49:37 +0200

    Upload image step

--- images-drawer-react/src/components/ImageUpload.jsx
+++ images-drawer-react/src/components/ImageUpload.jsx
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import {storage} from '../firebase';
 import {database} from '../firebase';
-import Gallery from 'react-grid-gallery';
 import ImageUploader from 'react-images-upload';
 
 class ImageUpload extends Component {
@@ -10,80 +9,15 @@ class ImageUpload extends Component {
         this.state = {
             image: null,
             images: [],
-            url: '',
-            progress: 0
+            url: ''
         }
         this.onDrop = this.onDrop.bind(this);
-    }
-
-    getData(newArray){
-        setTimeout(() => {
-            this.setState({
-                images: newArray
-            })
-        }, 10000)
-    }
-
-    addImageWithTags(doc, newArray, url){
-        var tags = [];
-        for (var label in doc.data().labels) {
-            tags.push({value: doc.data().labels[label].description, title: doc.data().labels[label].description});
-        }
-        newArray.push({src: url,thumbnail: url,thumbnailWidth: 320, thumbnailHeight: 212, tags: tags});
-    }
-
-    componentDidMount(){
-        var newArray = this.state.images.slice();
-        var imageRef = database.collection('images');
-        imageRef.get()
-            .then(snapshot => {
-                snapshot.forEach(doc => {
-                    storage.ref().child(doc.data().name).getDownloadURL().then(url => {
-                        this.addImageWithTags(doc, newArray, url);
-                    });
-                });
-            })
-            .catch(err => {
-                console.log('Error getting documents', err);
-            });
-        this.getData(newArray);
-    }
-
-    insertImage(image){
-        var imageRef = database.collection('images');
-        var doc = imageRef.where('name', '==',image.name).limit(1)
-        doc.onSnapshot(docSnapshot => {
-            docSnapshot.docs.forEach(elem => {
-                    storage.ref().child(image.name).getDownloadURL().then(url => {
-                        this.setState({url})
-                        var newArray = this.state.images.slice();
-                        this.addImageWithTags(elem, newArray, url);
-                        this.setState({images:newArray});
-                    });
-                }
-            );
-        });
     }
 
     onDrop(picture) {
         const image = picture[Object.keys(picture).length - 1];
         this.setState(() => ({image}));
-        const uploadTask = storage.ref(`${image.name}`).put(image);
-        uploadTask.on('state_changed',
-            (snapshot) => {
-                // progrss function ....
-                const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-                this.setState({progress});
-            },
-            (error) => {
-                // error function ....
-                console.log(error);
-            },
-            () => {
-                // complete function ....
-                this.setState({image});
-                this.insertImage(image)
-            });
+        const uploadTask = // TO DO
     }
 
     render() {
@@ -96,9 +30,6 @@ class ImageUpload extends Component {
         return (
             <React.Fragment>
                 <div style={style}>
-                    <br/>
-                    <progress value={this.state.progress} max="100"/>
-                    <br/>
                     <ImageUploader
                         withIcon={true}
                         buttonText='Choose images'
@@ -106,9 +37,6 @@ class ImageUpload extends Component {
                         imgExtension={['.jpg', '.gif', '.png', '.jpeg']}
                         maxFileSize={5242880}
                     />
-                </div>
-                <div id="example-0" className="center">
-                    <Gallery images={this.state.images} />
                 </div>
             </React.Fragment>
         )
--- images-drawer-react/src/firebase/index.js
+++ images-drawer-react/src/firebase/index.js
@@ -2,22 +2,6 @@ import firebase from 'firebase/app';
 import 'firebase/storage';
 import 'firebase/firestore';
 
-// Initialize Firebase
-  var config = {
-    apiKey: "",
-    authDomain: "",
-    databaseURL: "",
-    projectId: "",
-    storageBucket: "",
-    messagingSenderId: "",
-    appId: ""
-  };
-firebase.initializeApp(config);
+// TO DO Initialize config Firebase
 
-const storage = firebase.storage();
-
-export {
-    storage, firebase as default
-}
-
-export const database = firebase.firestore();
+export const storage = // TO DO 
